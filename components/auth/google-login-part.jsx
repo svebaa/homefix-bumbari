da5fb180478eb512904f3af97c724c8e09ea60ff
Lovro Milišić <lovro.milisic@gmail.com>
2025-10-18 15:59:19 +0200

    Implementiranje funkcionalnosti google ouath 2.0
    Fixes #2

--- components/auth/google-login-part.jsx
+++ components/auth/google-login-part.jsx
@@ -1,10 +1,24 @@
 "use client";
 
+import { login } from "@/lib/actions/auth-actions";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { FaGoogle } from 'react-icons/fa';
 
 export function GoogleLoginPart(parameters) {
+    
+    async function handleGoogle() {
+        const res = await login("google");
+
+        if (res?.error) {
+            console.error("OAuth error:", res.error);
+            return;
+        }
+
+        if (res?.url) {
+            window.location.href = res.url;
+        }
+    }
     return (
         <div className="flex-col py-4 space-y-4">
             <div className="flex items-center space-x-2">
@@ -17,7 +31,7 @@ export function GoogleLoginPart(parameters) {
                 </div>
             </div>
             <div>
-            <Button variant="outline" size="lg" className="w-full">
+            <Button variant="outline" size="lg" className="w-full" onClick={handleGoogle} disabled={parameters.loading}>
                 <FaGoogle />
                 {parameters.loading ? "Prijava u tijeku..." : "Google"}
             </Button>
